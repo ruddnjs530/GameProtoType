c8add0e0c909042ca0c0665f768621484a55270e
ruddnjs530 <kwk000530@naver.com>
2024-04-01 13:31:47 +0900

    Boss FSM 완성

--- My project/Assets/Script/BossEnemy.cs
+++ My project/Assets/Script/BossEnemy.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public enum BossState { Idle, Die, Attack1, Attack2, Move}
+public enum BossState { Idle, Die, Attack1, Attack2, MoveToPlayer}
 public class BossEnemy : MonoBehaviour
 {
     BossState bossState;
@@ -17,10 +18,26 @@ public class BossEnemy : MonoBehaviour
 
     float attackDamage = 10.0f;
 
-    float timer;
+    float timer = 0.0f;
 
     public GameObject textObject;
-    float hp = 100;
+    float hp = 20;
+
+    float walkSpeed = 2f;
+
+    Animator anim;
+
+    NavMeshAgent agent;
+
+    float jumpAttackCoolTime = 6f; 
+    float eightWayAttackCoolTime = 4f;
+
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform eightWayShotPos;
+
+    [SerializeField] AnimationCurve HeightCurve;
+    float jumpSpeed = 2f;
+    int jumpDamage = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -28,83 +45,214 @@ public class BossEnemy : MonoBehaviour
         bossState = BossState.Idle;
         laserLine = GetComponent<LineRenderer>();
 
-        timer = 0.0f;
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+
         laserLine.enabled = false;
+
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         switch (bossState)
-        {
+        { 
             case BossState.Idle:
-                if (canAttack)
+                anim.SetBool("isIdle", true);
+                if (hp <= 0)
                 {
-                    if (hp >30)
-                    {
-                        bossState = BossState.Attack1;
-                        break;
-                    }
-                    else
+                    bossState = BossState.Die;
+                    break;
+                }
+                if (hp <= 30)
+                {
+                    if (CanExecuteJumpAttack() || CanExecuteEightWayAttack())
                     {
                         bossState = BossState.Attack2;
                         break;
                     }
-
                 }
-                if (hp <= 0) bossState = BossState.Die;
-                break;
+                if (canAttack)
+                {
+                    bossState = BossState.Attack1;
+                    break;
+                }
+                else
+                {
+                    bossState = BossState.MoveToPlayer;
+                    break;
+                }
 
-            case BossState.Die:
-                Destroy(this.gameObject);
+            case BossState.MoveToPlayer: 
+                 Move();
+
+                if (hp <= 0)
+                {
+                    bossState = BossState.Die;
+                    break;
+                }
+                if (canAttack)
+                {
+                    bossState = BossState.Attack1;
+                    break;
+                }
+                if (hp <= 30)
+                {
+                    if (CanExecuteJumpAttack() || CanExecuteEightWayAttack())
+                    {
+                        bossState = BossState.Attack2;
+                        break;
+                    }
+                }
                 break;
 
             case BossState.Attack1:
                 timer += Time.deltaTime;
                 if (timer > fireRate)
                 {
-                    StartCoroutine(Attack());
+                    StartCoroutine(LaserAttack());
                     timer = 0.0f;
                 }
-                if (hp <= 0) bossState = BossState.Die;
-                if (!canAttack) bossState = BossState.Idle;
+
+                if (hp <= 0)
+                {
+                    bossState = BossState.Die;
+                    break;
+                }
+                if (hp <= 30)
+                {
+                    if (CanExecuteJumpAttack() || CanExecuteEightWayAttack())
+                    {
+                        bossState = BossState.Attack2;
+                        break;
+                    }
+                }
+                if (!canAttack)
+                {
+                    bossState = BossState.MoveToPlayer;
+                    break;
+                }
                 break;
 
             case BossState.Attack2:
-                if (hp <= 0) bossState = BossState.Die;
-                if (!canAttack) bossState = BossState.Idle;
+                if (CanExecuteJumpAttack())
+                {
+                    StartCoroutine(JumpAttack(target.position));
+                    jumpAttackCoolTime = Time.time + jumpAttackCoolTime;
+                    break;
+                }
+                else if (CanExecuteEightWayAttack())
+                {
+                    StartCoroutine(EightWayAttack());
+                    eightWayAttackCoolTime = Time.time + eightWayAttackCoolTime;
+                    break;
+                }
+                if (hp <= 0)
+                {
+                    bossState = BossState.Die;
+                    break;
+                }
+                if (!canAttack)
+                {
+                    bossState = BossState.MoveToPlayer;
+                    break;
+                }
+                break;
+
+            case BossState.Die:
+                anim.SetBool("isDie", true);
+                Destroy(this.gameObject, 3f);
                 break;
         }
     }
 
-    IEnumerator Attack()
+    private void Move()
+    {
+        agent.SetDestination(target.position);
+        agent.speed = walkSpeed;
+        anim.SetBool("isWalking", true);
+    }
+
+    IEnumerator JumpAttack(Vector3 targetPosition)
+    {
+        anim.SetTrigger("jumpAttack");
+        yield return new WaitForSeconds(0.5f);     
+        Vector3 startingPos = transform.position;
+        for (float time = 0; time < 1; time += Time.deltaTime * jumpSpeed)
+        {
+            transform.position = Vector3.Lerp(startingPos, targetPosition, time) + Vector3.up * HeightCurve.Evaluate(time);
+
+            yield return null;
+        }
+
+        float distance = Vector3.Distance(this.transform.position, target.position);
+        if (distance <= 10f) target.GetComponent<Player>().TakeDamage(jumpDamage);
+    }
+
+    IEnumerator LaserAttack()
     {
         laserLine.enabled = true;
         laserLine.SetPosition(0, shotPos.position);
         RaycastHit hit;
         Vector3 startPosition = shotPos.position;
 
-        if (Physics.Raycast(startPosition, transform.forward, out hit, laserRange))
+        if (Physics.Raycast(startPosition, transform.forward + (transform.up * -1), out hit, laserRange)) 
         {
             laserLine.SetPosition(1, hit.point);
             if (hit.transform.tag == "Player")
                 hit.transform.gameObject.GetComponent<Player>().TakeDamage(attackDamage);
         }    
         else
-            laserLine.SetPosition(1, startPosition + (transform.forward * laserRange));
+            laserLine.SetPosition(1, startPosition + (transform.forward + (transform.up * -1)) * laserRange); 
 
         yield return new WaitForSeconds(laserDuration);
         laserLine.enabled = false;
         canAttack = false;
     }
 
+    IEnumerator EightWayAttack()
+    {
+        anim.SetTrigger("eightWayAttack");
+        yield return new WaitForSeconds(1f);
+        Vector3[] directions = {
+            Vector3.forward,
+            Vector3.back,
+            Vector3.left,
+            Vector3.right,
+            (Vector3.forward + Vector3.right).normalized,
+            (Vector3.forward + Vector3.left).normalized,
+            (Vector3.back + Vector3.right).normalized,
+            (Vector3.back + Vector3.left).normalized
+        };
+
+        foreach (Vector3 direction in directions)
+        {
+            GameObject sphere = Instantiate(bulletPrefab, eightWayShotPos.position, Quaternion.identity);
+            Rigidbody rb = sphere.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddForce(direction * 10f, ForceMode.VelocityChange);
+            }
+        }
+    }
+
+    bool CanExecuteJumpAttack()
+    {
+        return Time.time >= jumpAttackCoolTime;
+    }
+
+    bool CanExecuteEightWayAttack()
+    {
+        return Time.time >= eightWayAttackCoolTime;
+    }
+
     private void OnTriggerStay(Collider other)
     {
-        //if (other.gameObject.tag != "Player") return;
-        if (other.gameObject.tag == "Player" && !canAttack)
+        if (other.gameObject.tag == "Player")
         {
-            target = other.transform;
-            transform.LookAt(target);
+            //shotPos.LookAt(other.transform); // 이걸 빼면 레이저가 제대로 안나감
+            transform.LookAt(other.transform);
             canAttack = true;
         }
     }
@@ -113,7 +261,6 @@ public class BossEnemy : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             canAttack = false;
-            target = null;
         }
     }
 
@@ -122,6 +269,7 @@ public class BossEnemy : MonoBehaviour
         hp -= damage;
         GameObject text = Instantiate(textObject, MakeRandomPosition(), Quaternion.identity);
         text.GetComponent<DamageText>().damage = damage;
+        anim.SetTrigger("isHit");
     }
 
     Vector3 MakeRandomPosition()
@@ -134,3 +282,102 @@ public class BossEnemy : MonoBehaviour
         return textPosition;
     }
 }
+
+
+//void EightWayAttack()
+//{
+//    anim.SetTrigger("eightWayAttack");
+
+//    Vector3[] directions = {
+//            Vector3.forward,
+//            Vector3.back,
+//            Vector3.left,
+//            Vector3.right,
+//            (Vector3.forward + Vector3.right).normalized,
+//            (Vector3.forward + Vector3.left).normalized,
+//            (Vector3.back + Vector3.right).normalized,
+//            (Vector3.back + Vector3.left).normalized
+//        };
+
+//    foreach (Vector3 direction in directions)
+//    {
+//        GameObject sphere = Instantiate(bulletPrefab, transform.position, Quaternion.identity); // 여기 transform.position말고 bulletShotPos로 해서 생성해야할듯
+//        Rigidbody rb = sphere.GetComponent<Rigidbody>();
+//        if (rb != null)
+//        {
+//            rb.AddForce(direction * 10f, ForceMode.VelocityChange);
+//        }
+//    }
+//}
+
+
+//IEnumerator EeightWayAttack()
+//{
+//    Vector3[] directions = { // 이걸 굳이 매번 받아야할까? 보스가 회전하니까 매번 받아야하나?
+//        Vector3.forward,
+//        Vector3.back,
+//        Vector3.left,
+//        Vector3.right,
+//        (Vector3.forward + Vector3.right).normalized,
+//        (Vector3.forward + Vector3.left).normalized,
+//        (Vector3.back + Vector3.right).normalized,
+//        (Vector3.back + Vector3.left).normalized
+//    };
+
+//    foreach (Vector3 direction in directions)
+//    {
+//        GameObject sphere = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+//        Rigidbody rb = sphere.GetComponent<Rigidbody>();
+//        if (rb != null)
+//        {
+//            rb.AddForce(direction * 10f, ForceMode.VelocityChange);
+//        }
+//    }
+//    yield return new WaitForSeconds(3f);
+//    canAttack = false;
+//}
+
+
+//void JumpAttack2() // 위에꺼 처럼 하고 네비메쉬로 이동하는걸로 수정해봐야할듯
+//{
+//    //float lerpRatio = timer / lerpTime;
+//    //Vector3 positionOffset = HeightCurve.Evaluate(lerpRatio) * lerpOffset;
+
+//    //transform.position = Vector3.Lerp(transform.position, target.position, lerpRatio) + positionOffset;
+
+//    for (float time = 0; time < 1; time += Time.deltaTime * jumpSpeed)
+//    {
+//        this.transform.position = Vector3.Lerp(this.transform.position, target.position, time)
+//            + Vector3.up * HeightCurve.Evaluate(time);
+//    }
+//    useTime = Time.time;
+//}
+
+//IEnumerator JumpAttack(Vector3 targetPostion)
+//{
+//    for (float time = 0; time < 1; time += Time.deltaTime * jumpSpeed)
+//    {
+//        this.transform.position = Vector3.Lerp(this.transform.position, targetPostion, time)
+//            + Vector3.up * HeightCurve.Evaluate(time);
+//        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.position - transform.position),
+//        //    time);
+
+//        //transform.position =new Vector3(target.position.x, HeightCurve.Evaluate(time), target.position.z);
+
+//        //this.transform.position = new Vector3(Vector3.Lerp(this.transform.position, target.position, time).x,
+//        //Vector3.Lerp(this.transform.position, target.position, time).y, Vector3.Lerp(this.transform.position, target.position, time).z) 
+//        //    +Vector3.up * HeightCurve.Evaluate(time);
+
+//        yield return null;
+//    }
+
+//    useTime = Time.time;
+
+//    float distance = Vector3.Distance(this.transform.position, target.position);
+//    //if (distance <= 2f) target.GetComponent<Player>().TakeDamage(jumpDamage);
+
+//    //if (NavMesh.SamplePosition(target.position, out NavMeshHit hit, 1f, this.agent.areaMask))
+//    //{
+//    //    agent.Warp(hit.position);
+//    //}
+//}
